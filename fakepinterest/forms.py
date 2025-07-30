acf32bffb34176f46dd090a103cf0d33b3f1cdaf
gzmoraes <moraesguh28@gmail.com>
2025-07-30 10:49:25 -0300

    Desenvolvimento de formulário e implementação das funcionalidades de Login e Cadastro.

---
+++ fakepinterest/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+class Form_sign_in(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirm = SubmitField("Entrar")
+
+class Form_login(FlaskForm):
+    username = StringField("Nome de usuário", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    senha_confirmacao = PasswordField("Confirme a senha", validators=[DataRequired(), EqualTo('senha')])
+    botao_confirm = SubmitField("Criar conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError("Email já cadastrado. Por favor, escolha outro.")
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,10 +1,17 @@
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest.forms import Form_sign_in, Form_login
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template("index.html")
+    sign_in = Form_sign_in()
+    return render_template("homepage.html", form=sign_in)
+
+@app.route('/login', methods=['GET', 'POST'])
+def create_account():
+    login = Form_login()
+    return render_template("login.html", form=login)
 
 @app.route('/perfil/<username>')
 @login_required
